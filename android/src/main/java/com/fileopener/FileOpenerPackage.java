6d94b8251624eac92d0354963cd07998c9b79e38
Alberto Blanco <alberto@basenet.nl>
2017-09-15 10:24:09 +0200

    RN 0.47 Breaking Change: No longer need to override this method.

--- android/src/main/java/com/fileopener/FileOpenerPackage.java
+++ android/src/main/java/com/fileopener/FileOpenerPackage.java
@@ -1,7 +1,6 @@
 package com.fileopener;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -16,11 +15,6 @@ public class FileOpenerPackage implements ReactPackage {
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     //Registering the module.
     return Arrays.<NativeModule>asList(new FileOpener(reactContext));
-  }
-
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
   }
 
   @Override
